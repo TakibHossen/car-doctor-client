dc6055ccda01dfb981f0a532ba056e1dfc4a2f1e
TakibHossen <mdtakibhossen1@gmail.com>
2023-05-14 00:50:24 +0600

    nav baner about implement

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,13 @@
+import About from "../About/About";
+import Baner from "../Baner/Baner";
+
+const Home = () => {
+    return (
+        <div>
+            <Baner></Baner>
+            <About></About>
+        </div>
+    );
+};
+
+export default Home;
