bfe0ff8e64364e0557a60cf61a0aad759607fb4b
TakibHossen <mdtakibhossen1@gmail.com>
2023-05-14 22:36:54 +0600

    login and singup firebase implement

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 import About from "../About/About";
 import Baner from "../Baner/Baner";
+import Services from "../Services/Services";
 
 const Home = () => {
     return (
         <div>
             <Baner></Baner>
             <About></About>
+            <Services></Services>
         </div>
     );
 };
